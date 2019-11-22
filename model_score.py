57c804c95019e3b690f29eaa6a3a9e1ab1187c22
Kenny Flagg <kenny@atrium.ai>
2019-11-22 11:45:57 -0700

    Initial commit.

---
+++ model_score.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sqlalchemy import create_engine
+import os
+
+def Extract_Data():
+    data_uri = 'postgres://vuvbqndrpsfoqn:64c1044b0c75aca863bf219187309088da53c1ee941f974e4fca2f09cf0e5381@ec2-174-129-222-15.compute-1.amazonaws.com:5432/dd3rjsf0sci85v'
+    engine = create_engine(data_uri)
+    dat = pd.read_sql('''SELECT Name, Petal_Length__c, Petal_Width__c, Sepal_Length__c, Sepal_Width__c, Species__c
+                         FROM salesforce.Iris__c''', engine)
+    engine.dispose()
+    return dat
+
+dat = Extract_Data()
+
+dat['pred'] = dat['petal_length__c'] * 2
+dat
+
+pickle.dump(dat, open('iris2.sav', 'wb'))
